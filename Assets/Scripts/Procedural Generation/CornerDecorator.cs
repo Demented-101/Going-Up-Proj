701db1aa14e698cb60648e96cc40b04c30878647
Demented <115271904+Demented-101@users.noreply.github.com>
2025-10-29 14:15:20 +0000

    Added Side Offices + Carpet
    - Added Hall Variation System to corners
    - Added Shader/Material to the floors (carpet)

--- Assets/Scripts/Procedural Generation/CornerDecorator.cs
+++ Assets/Scripts/Procedural Generation/CornerDecorator.cs
@@ -7,27 +7,40 @@ public class CornerDecorator : MonoBehaviour, Decorator
     GenObj generator;
 
     [SerializeField] private GameObject hallObject;
-    [SerializeField] private GameObject WallObject;
+    [SerializeField] private GameObject wallObject;
+    [SerializeField] private GameObject[] hallSideRoomObjects;
 
     public void Decorate(GenObj genObj)
     {
         generator = genObj;
         if (generator.connections.Count < 2) { return; } // requires at least two connections
 
+        int spawnOfficeDirection = Random.Range(0, generator.connections.Count + 2);
+
         List<Vector2Int> remainingDirections = new List<Vector2Int> { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
 
-        foreach (Vector2Int hallDirection in generator.connections)
+        for (int i = 0; i < generator.connections.Count; i++)
         {
+            Vector2Int hallDirection = generator.connections[i];
             remainingDirections.Remove(hallDirection);
 
-            GameObject newHall = Instantiate(hallObject, transform.position, Quaternion.identity);
+            // select hall object to spawn
+            GameObject selectedHall = hallObject;
+            if(spawnOfficeDirection == i)
+            {
+                selectedHall = hallSideRoomObjects[Random.Range(0, hallSideRoomObjects.Length)];
+            }
+
+            // spawn new hall
+            GameObject newHall = Instantiate(selectedHall, transform.position, Quaternion.identity);
             newHall.transform.parent = transform;
             newHall.transform.LookAt(transform.position + new Vector3(hallDirection.y, 0, -hallDirection.x));
         }
 
         foreach (Vector2Int wallDirection in remainingDirections)
         {
-            GameObject newWall = Instantiate(WallObject, transform.position, Quaternion.identity);
+            // spawn wall
+            GameObject newWall = Instantiate(wallObject, transform.position, Quaternion.identity);
             newWall.transform.parent = transform;
             newWall.transform.LookAt(transform.position + new Vector3(-wallDirection.y, 0, wallDirection.x));
         }
